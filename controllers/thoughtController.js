06d0ac3ef3c5642a73bf2994c39701589d8d0d6f
camilleramos1 <129894673+camilleramos1@users.noreply.github.com>
2023-08-28 13:34:08 -0600

    added thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,67 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    // get all thoughts
+    getAllThought(req, res) {
+        Thought.find({})
+        .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // get one thought
+    getOneThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .then((thought) => {
+            if (!thought) {
+                return res.status(404).json({ message: "Thought not found" })
+            }
+            res.json(thought);
+        })
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // create a thought
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thought) => {
+            return User.findOneAndUpdate(
+                { _id: thought.username },
+                { $push: {thoughts: thought._id } },
+                { new: true }
+            );
+        }).then(() => res.json({ message: "Thought created!" }))
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // update a thought
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { new: true }
+        )
+        .then((thought) => {
+            if (!thought) {
+                return res.status(404).json({ message: "Thought not found" })
+            }
+            res.json(thought);
+        })
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // delete a thought
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+        .then((thought) => {
+            if (!thought) {
+                return res.status(404).json({ message: "Thought not found" })
+            }
+
+            return User.findOneAndUpdate(
+                { _id: thought.userId },
+                { $pull: { thoughts: thought._id } }
+            );
+        })
+        .then(() => res.json({ message: "Thought deleted"}))
+    }
+}
